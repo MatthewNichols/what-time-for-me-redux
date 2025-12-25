33ca2a860cec4dcac4dcf59b6029757048ad2aa9
MatthewNichols <matthew@matthew-nichols.com>
2025-12-25 18:57:35 +0100

    Hopefully handeled my config issue

--- src/11ty-extensions/get-environment-vars.js
+++ src/11ty-extensions/get-environment-vars.js
@@ -1,4 +1,8 @@
-import 'dotenv/config';
+import path from 'path';
+import dotenv from 'dotenv';
+
+const envFile = `.env.${process.env.ELEVENTY_ENV || 'development'}`;
+dotenv.config({ path: path.resolve(process.cwd(), envFile) });
 
 /**
  * Registers a fun
@@ -12,10 +16,6 @@ export function registerGetEnvironmentVars(eleventyConfig) {
       AVAILABLE_START_HOUR, 
       AVAILABLE_END_HOUR 
     } = process.env;
-
-    console.log("Injecting environment vars to clientside:", {
-      ...process.env
-    });
 
     return `window.CLIENTSIDE_CONFIG_VARS = ${JSON.stringify( {
       MY_LOCATION_NAME,
